901dc4ec15c79f12755afbab6a8faa51c758d7c9
mossa <martin.ossa.8@gmail.com>
2023-02-21 17:32:02 +0100

    Setup live server

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,21 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-  ],  
+  ],
+  devServer: {
+    static: './dist',
+  },  
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  }
 };
