0497bf032bb8f951165bcc542ca06ca49a64c640
Martin Ossa <108192399+maov19@users.noreply.github.com>
2023-02-22 15:04:58 +0100

    delete mode: none from module.exports

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'none',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
