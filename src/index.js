4d068330bac26173499b4cf211965631fa9bd608
mossa <martin.ossa.8@gmail.com>
2023-02-22 11:25:35 +0100

    Add dynamic list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import './style.css';
 
+const displayList = document.getElementById('todolist');
 
 const toDoList = [
   {
     description: 'wash the dishes',
     completed: false,
-    index: 0,
+    index: null,
   },
   {
     description: 'complete To Do list project',
     completed: false,
-    index: 0,
-  },
+    index: null,
+  }
 ]
 
+for (const item in toDoList) {
+  displayList.innerHTML += `
+    <div class="task ${toDoList[item].description}">
+    <input class="task status" type="checkbox">
+    <p class="task description">${toDoList[item].description}</p>
+    <span class="icon"></span>
+    </div>
+    `
+}
